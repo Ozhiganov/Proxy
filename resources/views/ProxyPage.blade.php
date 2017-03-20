f5fcc8f2ad56dae55104ace369f94ebf4773e148
Dominik Pfennig <dominik@suma-ev.de>
2017-03-20 11:52:21 +0100

    Header neu formatiert

--- resources/views/ProxyPage.blade.php
+++ resources/views/ProxyPage.blade.php
@@ -8,19 +8,27 @@
 		<nav>
 			<div class="container-fluid">
 				<div class="row">
-					<div id="proxy-logo" class="col-xs-2">
-						<img src="/img/mglogo_klein.png" />
+					<div id="proxy-logo" class="visible-lg col-lg-2">
+						<a href="https://metager.de">
+							<img src="/img/mglogo_klein.png" />
+						</a>
 					</div>
-					<div id="proxy-text" class="col-xs-8">
+					<div id="proxy-text" class="col-xs-6 col-lg-5">
 						<ul class="list-unstyled">
-							<li>MetaGer/SUMA-EV Proxy (Beta): Anonymisiert & verschlüsselt. SUMA-EV ist weder Eigentümer noch Urheber von Inhalten.</li>
+							<li><nobr>MetaGer/SUMA-EV Proxy (Beta): Anonymisiert & verschlüsselt.</nobr> <nobr>SUMA-EV ist weder Eigentümer noch Urheber von Inhalten.</nobr></li>
 							<li>Skripte sind deaktiviert. Webseiten-Darstellung kann verändert sein.</li>
+							<li><a href="{!!$targetUrl!!}" class="btn btn-danger btn-xs">Proxy ausschalten</a></li>
 						</ul>
 					</div>
-					<div id="proxy-options" class="col-xs-2">
-						<ul class="list-unstyled">
-						<li><a href="{!!$targetUrl!!}" class="btn btn-danger btn-xs">Proxy ausschalten</a></li>
-						</ul>
+					<div id="proxy-advertisement" class="col-xs-6 col-lg-5">
+					<a href="http://metager.to/k25u0" target="_blank">
+						<span class="ad-marker">Anzeige</span>
+						<p class="heading">Samsung Galaxy S7 im Preisvergleich</p>
+						<p class="url">www.idealo.de/preisvergleich/samsung-galaxy-s7/</p>
+						<p class="line">Jetzt Samsung Galaxy S7 ohne Vertrag zum günstigsten Preis finden!</p>
+						<p class="line">Große Shopvielfalt, Testberichte & Meinungen. Jetzt auf idealo.de</p>
+					</a>
+
 					</div>
 				</div>
 			</div>

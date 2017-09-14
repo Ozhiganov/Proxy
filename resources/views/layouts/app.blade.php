769636845efb89d42f0a6d1c21d4c085c6b7b7f6
Dominik Pfennig <dominik@suma-ev.de>
2017-09-14 16:15:48 +0200

    Bootstrap entfernt

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,6 @@
 <head>
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-<link href="/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
 <link href="{{ mix('/css/all.css') }}" rel="stylesheet" type="text/css" />
 </head>
 <body>

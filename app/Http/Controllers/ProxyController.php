ca91812233e4a171f3142c2d52bc4b26c9dca742
Dominik Pfennig <dominik@suma-ev.de>
2018-01-26 10:11:27 +0100

    Relative Links aus einem Redirect werden nun korrekt aufgelöst

--- app/Http/Controllers/ProxyController.php
+++ app/Http/Controllers/ProxyController.php
@@ -325,7 +325,12 @@ class ProxyController extends Controller
                         } elseif (!$withCookies && strtolower($ar[0]) === "content-type") {
                             $headerArray[strtolower(trim($ar[0]))] = strtolower(trim($ar[1]));
                         } elseif (strtolower($ar[0]) === "location") {
-                            $headerArray[trim($ar[0])] = $this->proxifyUrl(trim($ar[1]), null, false);
+                            $redLink = $ar[1];
+                            if(strpos($redLink, "/") === 0){
+                                $parse = parse_url($url);
+                                $redLink = $parse["scheme"] . "://" . $parse["host"] . $redLink;
+                            }
+                            $headerArray[trim($ar[0])] = $this->proxifyUrl($redLink, null, false);
                         } elseif (strtolower($ar[0]) === "content-disposition") {
                             $headerArray[strtolower(trim($ar[0]))] = strtolower(trim($ar[1]));
                         } else {

b112d18290d067248a06686b7f9e20ced43dc86a
Dominik Hebeler <dominik@suma-ev.de>
2019-08-30 12:01:37 +0200

    Added load balancer to trusted proxies

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Middleware;
 
-use Illuminate\Http\Request;
 use Fideloper\Proxy\TrustProxies as Middleware;
+use Illuminate\Http\Request;
 
 class TrustProxies extends Middleware
 {
@@ -12,7 +12,9 @@ class TrustProxies extends Middleware
      *
      * @var array|string
      */
-    protected $proxies;
+    protected $proxies = [
+        '10.108.160.148',
+    ];
 
     /**
      * The headers that should be used to detect proxies.
